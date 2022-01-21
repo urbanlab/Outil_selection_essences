1f0d721f831fa7bac8dbb853ab9baf562f0c9289
marjoriehitchon <marjorie.hitchon@gmail.com>
2022-01-21 10:14:24 +0100

    Filtres

--- app.js
+++ app.js
@@ -14,13 +14,39 @@ app.get('/styles/style.css', (req, res) => {
     res.status(200).sendFile(__dirname + '/styles/style.css');
 });
 
-app.get('/test', (req, res)=>{
-    const p = gsheet.getData(gsheet.client, `'Paramétrages critères'!A:Q`)
+//========================== Filtres ==============================
+app.get('/filtres', (req, res)=>{
+    const p = gsheet.getData(gsheet.client, `'Paramétrages critères'!A:Q`);
     p.then((value)=>{
-        res.send(JSON.stringify(value))
+        liste_criteres=[];
+        for (let i = 1; i < value[0].length; i++) {
+            if (value[2][i] == 'TRUE'){
+                var json = {
+                    nom: value[0][i],
+                    importance: value[1][i],
+                    type_question: value[3][i]
+                }
+                var c=4;
+                reponses=[];
+                while (value[c] && value[c][i]!='') {
+                    morceaux = value[c][i].split(':');
+                    json_reponse = {
+                        valeur: morceaux[0],
+                        texte: morceaux[1]
+                    }
+                    reponses.push(json_reponse);
+                    c++;
+                }
+                json['reponses']=reponses;
+                liste_criteres.push(json);
+            }
+        }
+        res.send(JSON.stringify(liste_criteres));
     })
     .catch((err)=>{
-        console.log(err)
+        console.log(err);
     })
-})
+});
+//========================== Filtres ==============================
+
 module.exports = app;
