0cd893376aff6f8c71d4321c569102af3416fef3
Ugo Insalaco <ugo.insalaco@hotmail.fr>
2022-01-21 10:12:17 +0100

    ajout jquery

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const gsheet = require("./gsheet.js")
+const utils = require("./utils")
 //-------------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------- Paramétrages de base -------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------------
@@ -23,4 +24,18 @@ app.get('/test', (req, res)=>{
         console.log(err)
     })
 })
+//==================== /data/arbres ==================
+app.get('/data/arbres', (req, res)=>{
+    gsheet.getData(gsheet.client, `'Tableau des essences'!A4:4`)
+    .then((colnames)=>{
+        ncols = colnames[0].length
+        lastColumn = baseDecomposition(26, ncols)
+        gsheet.getData(gsheet.client, `'Tableau des essences'!A6:4`)
+        res.send("c'est bon")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+// ===================================================
 module.exports = app;
