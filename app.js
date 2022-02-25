ced812e969a34983a8b9d36194122340e882b7f0
Ugo Insalaco <ugo.insalaco@hotmail.fr>
2022-02-25 15:15:30 +0100

    require bug

--- app.js
+++ app.js
@@ -79,21 +79,25 @@ app.get('/data/filtres', (req, res)=>{
 
 // ==================== /data/arbres =================
 app.post('/data/arbres', (req, res) => {
-    const param_page = req.query.page;
-    mydata=require('./data/arbres.json');
-    description=require('./data/filtres.json');
-    result_tri = compute_scores(mydata,description,req.body);
-    response = [];
+    const param_page = xss(req.query.page);
+    fs.readFile('./data/arbres.json', (err, mydata)=>{
+        mydata = JSON.parse(mydata)
+        fs.readFile('./data/filtres.json', (err, description)=>{
+            description = JSON.parse(description)
+            const result_tri = compute_scores(mydata,description,req.body);
+            let response = [];
 
-    const page = (param_page) ? parseInt(param_page) : 1;
+            const page = (param_page) ? parseInt(param_page) : 1;
 
-    for (let i = (page-1)*9; i < Math.min(page*9, result_tri.length); i++) {
-        let arbre = result_tri[i]
-        response.push(arbre);
-    }
-    var json = {nb_arbres_tries : result_tri.length,
-                response: response};
-    res.status(200).send(json);
+            for (let i = (page-1)*9; i < Math.min(page*9, result_tri.length); i++) {
+                let arbre = result_tri[i]
+                response.push(arbre);
+            }
+            var json = {nb_arbres_tries : result_tri.length,
+                        response: response};
+            res.status(200).send(json);
+        })
+    })
 });
 
 app.get('/data/arbres/random', (req, res)=>{
@@ -191,7 +195,7 @@ app.get("/secure/data/refresh", (req,res)=>{
         gsheet.getData(gsheet.client, `'${config.data_spreadsheet}'!${config.data_column_offset}${config.data_start_row}:${lastColumn}`)
         .then((values)=>{
             let response = []
-            for(let i = 0; i < values.length; i++) {
+            for(let i = 0; i < values.length-1; i++) {
                 let val = {}
                 for(let j=0; j<ncols; j++){
                     val[colnames[0][j]]=values[i][j]
